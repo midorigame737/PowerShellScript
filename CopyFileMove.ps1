59b0fb85a252437e5125ecdb7e2b54b61846f6bc
midorigame737 <midorigame6@gmail.com>
2022-02-24 15:14:49 +0900

    処理を行うディレクトリを指定するための引数設定した

---
+++ CopyFileMove.ps1
@@ -0,0 +1,13 @@
+﻿function global:MoveDuplicateNameFiles{
+    Param(
+        [String]$Arg1="./"
+    )
+    $CurrentDirectry=pwd
+    cd $Arg1
+    $Files=ls -Name
+    $MatchPattern=".*\(\d\).*"
+    $MatchFiles= $Files -match $MatchPattern
+    mkdir ./Copys
+    Move-Item $MatchFiles ./Copys
+    cd $CurrentDirectry
+}
