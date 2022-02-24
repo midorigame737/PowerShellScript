9b7ec75ad6b18e74f45124d13597efb220c3d02c
midorigame737 <midorigame6@gmail.com>
2022-02-24 16:18:16 +0900

    引数付けた

--- CopyFileMove.ps1
+++ CopyFileMove.ps1
@@ -1,5 +1,14 @@
-﻿$Files=ls -Name
-$MatchPattern=".*\(\d\).*"
-$MatchFiles= $Files -match $MatchPattern
-mkdir ./Copys
-Move-Item $MatchFiles ./Copys
+﻿function global:MoveDuplicateNameFiles{
+    Param(
+        [String]$Arg1="./"
+    )
+    [CmdletBinding()]
+    $CurrentDirectry=pwd
+    cd $Arg1
+    $Files=ls -Name
+    $MatchPattern=".*\(\d\).*"
+    $MatchFiles= $Files -match $MatchPattern
+    mkdir ./Copys
+    Move-Item $MatchFiles ./Copys
+    cd $CurrentDirectry
+}
